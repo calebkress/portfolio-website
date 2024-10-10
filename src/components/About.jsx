52da389d9e281599024032338007ac5f07865f52
Caleb Kress <calebkress@gmail.com>
2024-10-10 13:46:18 -0500

    implemented icons for tech stack

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,9 +21,33 @@ const About = () => {
         </p>
 
         <h2>Technologies I Work With</h2>
-        <div className="tech-stack">
+        {/* Python section */}
+        <div className="tech-group">
           <div className="tech-item">
-            <i className="devicon-react-original colored"></i>
+            <i className="devicon-python-plain colored"></i>
+            <span>Python</span>
+          </div>
+          <div className="tech-item">
+            <i className="devicon-pandas-plain-wordmark"></i>
+            <span>Pandas</span>
+          </div>
+          <div className="tech-item">
+            <i className="devicon-scikitlearn-plain colored"></i>
+            <span>scikit-learn</span>
+          </div>
+          <div className="tech-item">
+            <i className="devicon-numpy-plain colored"></i>
+            <span>numpy</span>
+          </div>
+        </div>
+        {/* JavaScript section */}
+        <div className="tech-group">
+          <div className="tech-item">
+            <i className="devicon-javascript-plain colored"></i>
+            <span>JavaScript</span>
+          </div>
+          <div className="tech-item">
+            <i className="devicon-react-plain colored"></i>
             <span>React</span>
           </div>
           <div className="tech-item">
@@ -31,12 +55,50 @@ const About = () => {
             <span>Node.js</span>
           </div>
           <div className="tech-item">
-            <i className="devicon-postgresql-plain colored"></i>
-            <span>PostgreSQL</span>
+            <i className="devicon-express-original"></i>
+            <span>Express.js</span>
           </div>
           <div className="tech-item">
-            <i className="devicon-python-plain colored"></i>
-            <span>Python</span>
+            <i className="devicon-d3js-plain colored"></i>
+            <span>D3.js</span>
+          </div>
+        </div>
+        {/* General tech/DBs section */}
+        <div className="tech-group">
+          <div className="tech-item">
+            <i className="devicon-html5-plain colored"></i>
+            <span>HTML5</span>
+          </div>
+          <div className="tech-item">
+            <i className="devicon-css3-plain colored"></i>
+            <span>CSS3</span>
+          </div>
+          <div className="tech-item">
+            <i className="devicon-bootstrap-plain colored"></i>
+            <span>Bootstrap</span>
+          </div>
+          <div className="tech-item">
+            <i className="devicon-mysql-plain colored"></i>
+            <span>MySQL</span>
+          </div>
+          <div className="tech-item">
+            <i className="devicon-mongodb-plain colored"></i>
+            <span>MongoDB</span>
+          </div>
+        </div>
+        {/* Tools */}
+        <div className="tech-group">
+          <div className="tech-item">
+            <i className="devicon-git-plain colored"></i>
+            <span>Git</span>
+          </div>
+          <div className="tech-item">
+            <i className="devicon-github-original"></i>
+            <span>Github</span>
+          </div>
+          <div className="tech-item">
+            <i className="devicon-amazonwebservices-plain-wordmark colored"></i>
+            <span>AWS</span>
           </div>
         </div>
       </div>
