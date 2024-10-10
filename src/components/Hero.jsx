0667b71b737d255f6011967ccc0f8dbe09bd5eb1
Caleb Kress <calebkress@gmail.com>
2024-10-10 15:57:24 -0500

    implemented lazy loading

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <section id="hero" className="hero-section">
       <div className="hero-content">
-        <img src="/profile.jpg" alt="Caleb Kress Profile Photo" className="hero-photo" />
+        <img src="/profile.jpg" alt="Caleb Kress Profile Photo" className="hero-photo" loading="lazy" />
         <div className="hero-text">
           <h1>Caleb Kress</h1>
           <p className="location">
