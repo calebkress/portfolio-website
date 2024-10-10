999805abbb252c7c2a226687c12025a314ccc3fb
Caleb Kress <calebkress@gmail.com>
2024-10-10 13:51:26 -0500

    rebase to main

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import Hero from './components/Hero';
 import About from './components/About';
 import Projects from './components/Projects';
 import Resume from './components/Resume';
@@ -9,6 +10,7 @@ const App = () => {
   return (
     <div>
       <Header />
+      <Hero />
       <About />
       <Projects />
       <Resume />
