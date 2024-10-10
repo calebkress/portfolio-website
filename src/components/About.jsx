6f2343c9ac55258be9c4412f29840d3c16e3d9cf
Caleb Kress <calebkress@gmail.com>
2024-10-10 12:39:45 -0500

    implemented about styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import './About.css';
 
 const About = () => {
   return (
-    <section id="about">
+    <section className="about">
       <h1>About Me</h1>
       <p>Hi, I’m Caleb Kress, a data scientist passionate about using Python and SQL to uncover insights and solve real-world problems.</p>
     </section>
